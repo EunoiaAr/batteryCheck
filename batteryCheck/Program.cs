6122c62f4900ff2c0dcf73d96f57206fa5b0a4ab
Eugenio Miró <eugenio.miro@gmail.com>
2016-09-24 14:32:49 -0300

    runs even if no battery is found on system

--- batteryCheck/Program.cs
+++ batteryCheck/Program.cs
@@ -7,15 +7,19 @@ namespace batteryCheck
     {
         static void Main(string[] args)
         {
-            // see: https://gist.github.com/ahawker/9715872
-            BatteryInformation cap = BatteryInfo.GetBatteryInformation();
+            try {
+                // see: https://gist.github.com/ahawker/9715872
+                BatteryInformation cap = BatteryInfo.GetBatteryInformation();
 
-            Console.WriteLine("cap PowerState {0}", cap.PowerState);
-            Console.WriteLine("cap discharge rate {0}", cap.DischargeRate);
-            Console.WriteLine("cap voltage {0}", cap.Voltage);
-            Console.WriteLine("cap FullChargeCapacity {0}", cap.FullChargeCapacity);
-            Console.WriteLine("cap DesignedMaxCapacity {0}", cap.DesignedMaxCapacity);
-            Console.WriteLine("cap CurrentCapacity {0}", cap.CurrentCapacity);
+                Console.WriteLine("cap PowerState {0}", cap.PowerState);
+                Console.WriteLine("cap discharge rate {0}", cap.DischargeRate);
+                Console.WriteLine("cap voltage {0}", cap.Voltage);
+                Console.WriteLine("cap FullChargeCapacity {0}", cap.FullChargeCapacity);
+                Console.WriteLine("cap DesignedMaxCapacity {0}", cap.DesignedMaxCapacity);
+                Console.WriteLine("cap CurrentCapacity {0}", cap.CurrentCapacity);
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
 
             // see: https://msdn.microsoft.com/en-us/library/windows/desktop/aa372693(v=vs.85).aspx
 
@@ -23,9 +27,11 @@ namespace batteryCheck
 
             Api.Kernel.GetSystemPowerStatus(sps);
 
-            Console.WriteLine("AC Line status {0}", sps.ACLineStatus);
-            Console.WriteLine("Battery flag {0}", sps.BatteryFlag);
-            Console.WriteLine("Battery Life Percent {0}", sps.BatteryLifePercent);
+            Console.WriteLine("SYSTEM_POWER_STATUS AC Line status {0}",         sps.ACLineStatus);
+            Console.WriteLine("SYSTEM_POWER_STATUS Battery flag {0}",           sps.BatteryFlag);
+            Console.WriteLine("SYSTEM_POWER_STATUS Battery Life Percent {0}",   sps.BatteryLifePercent);
+
+
         }
     }
 }
