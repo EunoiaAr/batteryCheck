affc8d09f809f8a80145fd0ac21e35b445890415
Eugenio Miró <eugenio.miro@gmail.com>
2016-09-24 11:21:22 -0300

    honor initial project's source

--- batteryCheck/Program.cs
+++ batteryCheck/Program.cs
@@ -4,9 +4,12 @@ namespace batteryCheck
 {
     class Program
     {
+        // see: https://gist.github.com/ahawker/9715872
+
         static void Main(string[] args)
         {
             BatteryInformation cap = BatteryInfo.GetBatteryInformation();
+
             Console.WriteLine("cap PowerState {0}", cap.PowerState);
             Console.WriteLine("cap discharge rate {0}", cap.DischargeRate);
             Console.WriteLine("cap voltage {0}", cap.Voltage);
